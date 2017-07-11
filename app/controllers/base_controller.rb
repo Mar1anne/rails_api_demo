b0291a4e0eab7a403ddc05a11e9334940466c5d1
Mariana <mariana.ristovska@gmail.com>
2017-07-11 13:29:08 +0200

    Added apipie, generated sample doc for user #show and #create

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,6 +1,9 @@
 class BaseController < ApplicationController
   include ApiError
 
+  resource_description do
+    api_version '1.0'
+  end
 
   # Error Handling
   rescue_from(Error) { |e| handle_error(e) }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,16 @@ class UsersController < BaseController
   # before_action authorize except sign in
   respond_to :json
 
+  def_param_group :user do
+    param :first_name, String, ''
+    param :last_name, String, ''
+    param :nickname, String, 'Must be unique', required: true, allow_nil: false
+    param :email, String, '', required: true, allow_nil: false
+  end
+
+  api! 'Renders user info for given user_id'
+  param :id, String, 'User id', required: true, allow_nil: false
+
   def show
     user = User.find_by_id(params[:id])
     raise NotFound.new('user_not_found', 'User not found!') if user.blank?
@@ -12,6 +22,9 @@ class UsersController < BaseController
   def index
     @users = User.all
   end
+
+  api! 'Creates a user'
+  param_group :user
 
   def create
     user = User.new user_params
