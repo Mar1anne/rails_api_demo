a80991fac99b38c7286686bec824f8ba55ff2ef1
Mariana <mariana.ristovska@gmail.com>
2017-07-12 17:30:46 +0200

    Added doorkeeper gem, added ability to register applications via active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   apipie
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   resources :users, only: [:index, :show, :create, :update, :destroy] do
     resources :posts, only: [:index, :show, :create, :update, :destroy]
   end
